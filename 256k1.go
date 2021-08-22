c449e3aa6b30bddd8dfac86cd4d4344bf9b11378
John Kemp <stable.pseudonym@gmail.com>
2021-08-22 16:07:39 -0700

    added IsOnCurve method, needed for unmarshal EC point after key gen

--- 256k1.go
+++ 256k1.go
@@ -26,3 +26,39 @@ func P256K1() elliptic.Curve {
 func (curve p256k1Curve) Params() *elliptic.CurveParams {
 	return curve.CurveParams
 }
+
+func (curve p256k1Curve) IsOnCurve(x, y *big.Int) bool {
+	// y² = x³ + b
+	y2 := new(big.Int).Mul(y, y) //y²
+	y2.Mod(y2, curve.Params().P)       //y²%P
+
+	x3 := new(big.Int).Mul(x, x) //x²
+	x3.Mul(x3, x)                //x³
+
+	x3.Add(x3, curve.Params().B) //x³+B
+	x3.Mod(x3, curve.Params().P) //(x³+B)%P
+
+	return x3.Cmp(y2) == 0
+}
+
+// func Unmarshal(curve elliptic.Curve, data []byte) (x, y *big.Int) {
+// 	byteLen := (curve.Params().BitSize + 7) / 8
+// 	if len(data) != 1+2*byteLen {
+// 		return nil, nil
+// 	}
+// 	if data[0] != 4 { // uncompressed form
+// 		return nil, nil
+// 	}
+// 	p := curve.Params().P
+// 	x = new(big.Int).SetBytes(data[1 : 1+byteLen])
+// 	y = new(big.Int).SetBytes(data[1+byteLen:])
+// 	if x.Cmp(p) >= 0 || y.Cmp(p) >= 0 {
+// 		log.Printf("Comparison failed for x or y")
+// 		return nil, nil
+// 	}
+// 	if !curve.IsOnCurve(x, y) {
+// 		log.Printf("IsOnCurve failed!")
+// 		return nil, nil
+// 	}
+// 	return
+// }
